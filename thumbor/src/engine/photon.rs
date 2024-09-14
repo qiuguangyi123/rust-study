18fdfe0b3f6613ca6eb998769c70d7052a54c762
“qiuguangyi” <qiugy@flatincbr.com>
2024-09-14 20:12:27 +0800

    fix: 添加 spec 实现

--- thumbor/src/engine/photon.rs
+++ thumbor/src/engine/photon.rs
@@ -46,3 +46,55 @@ impl Engine for Photon {
         image_to_buf(self.0, format)
     }
 }
+
+impl SpecTransform<&Crop> for Photon {
+    fn transform(&mut self, op: &Crop) {
+        let img = transform::crop(&mut self.0, op.x1, op.y1, op.x2, op.y2);
+        self.0 = img;
+    }
+}
+impl SpecTransform<&Contrast> for Photon {
+    fn transform(&mut self, op: &Contrast) {
+        effects::adjust_contrast(&mut self.0, op.contrast);
+    }
+}
+impl SpecTransform<&Flipv> for Photon {
+    fn transform(&mut self, _op: &Flipv) {
+        transform::flipv(&mut self.0)
+    }
+}
+impl SpecTransform<&Fliph> for Photon {
+    fn transform(&mut self, _op: &Fliph) {
+        transform::fliph(&mut self.0)
+    }
+}
+impl SpecTransform<&Filter> for Photon {
+    fn transform(&mut self, op: &Filter) {
+        match filter::Filter::from_i32(op.filter) {
+            Some(filter::Filter::Unspecified) => {}
+            Some(f) => filters::filter(&mut self.0, f.to_str().unwrap()),
+            _ => {}
+        }
+    }
+}
+impl SpecTransform<&Resize> for Photon {
+    fn transform(&mut self, op: &Resize) {
+        let img = match resize::ResizeType::from_i32(op.rtype).unwrap() {
+            resize::ResizeType::Normal => transform::resize(
+                &mut self.0,
+                op.width,
+                op.height,
+                resize::SampleFilter::from_i32(op.filter).unwrap().into(),
+            ),
+            resize::ResizeType::SeamCarve => {
+                transform::seam_carve(&mut self.0, op.width, op.height)
+            }
+        };
+        self.0 = img;
+    }
+}
+impl SpecTransform<&Watermark> for Photon {
+    fn transform(&mut self, op: &Watermark) {
+        multiple::watermark(&mut self.0, &WATERMARK, op.x, op.y);
+    }
+}
